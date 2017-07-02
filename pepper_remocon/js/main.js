f951688e98e1327f93ac117d26ca0d9f15096fbd
coil398 <konayuta.sou@gmail.com>
2017-07-02 15:29:37 +0900

    commit

--- pepper_remocon/js/main.js
+++ pepper_remocon/js/main.js
@@ -1,3 +1,5 @@
+const pi = 3.1415926;
+
 let pepperCon = {
     qiSession: null,
     alTextToSpeech: null,
@@ -23,6 +25,10 @@ const setServices = () => {
     });
 }
 
+const changeStatus = () => {
+    document.getElementById('status').innerHTML = '接続';
+}
+
 const testMethod = () => {
     console.log('btn clicked.');
     console.log(pepperCon);
@@ -36,6 +42,7 @@ pepperIPbtn.addEventListener('click', (e) => {
         return 0;
     }else{
         setServices();
+        changeStatus();
     }
 });
 
@@ -52,4 +59,49 @@ sayBtn.addEventListener('click', (e) => {
 const moveForward = document.getElementById('moveForward');
 moveForward.addEventListener('click', (e) => {
     pepperCon.alMotion.moveTo(0.5, 0, 0);
+});
+
+const moveBackward = document.getElementById('moveBackward');
+moveBackward.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(-0.5, 0, 0);
+});
+
+const moveRightward = document.getElementById('moveRightward');
+moveRightward.addEventListener('click', (e) => {
+        pepperCon.alMotion.moveTo(0, -0.5, 0);
+});
+
+const moveLeftward = document.getElementById('moveLeftward');
+moveLeftward.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(0, 0.5, 0);
+});
+
+const turnRight30 = document.getElementById('turnRight30');
+turnRight30.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(0, 0, -30*pi/180);
+});
+
+const turnRight60 = document.getElementById('turnRight60');
+turnRight60.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(0, 0, -60*pi/180);
+});
+
+const turnRight90 = document.getElementById('turnRight90');
+turnRight90.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(0, 0, -90*pi/180);
+});
+
+const turnLeft30 = document.getElementById('turnLeft30');
+turnLeft30.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(0, 0, 30*pi/180);
+});
+
+const turnLeft60 = document.getElementById('turnLeft60');
+turnLeft60.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(0, 0, 60*pi/180);
+});
+
+const turnLeft90 = document.getElementById('turnLeft90');
+turnLeft90.addEventListener('click', (e) => {
+    pepperCon.alMotion.moveTo(0, 0, 90*pi/180);
 });
