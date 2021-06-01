48eb1ef0c1cca8f47732105beabded2f713fe1e4
coil398 <konayuta.sou@gmail.com>
2021-06-01 22:29:58 +0900

    first commit

--- web/.next/static/rFKkEhLq7RoWOzGvYtji2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/115bd9a6a9ada811c579.css","static/chunks/pages/index-9a037546af7877225a3c.js"],"/_error":["static/chunks/pages/_error-3395569fecf98a24f01b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
